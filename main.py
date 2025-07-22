f28a71d1bed50222726956b0bdada8c37f1ee3d2
Denys Stefanko <denys.stefanko@gmail.com>
2025-07-22 13:17:07 +0300

    Add health check endpoint

--- main.py
+++ main.py
@@ -16,3 +16,8 @@ async def say_hello(name: str):
 @app.get("/test-endpoint")
 async def test_endpoint():
     return {"message": "This is a test endpoint."}
+
+
+@app.get("/health-check")
+async def health_check():
+    return {"status": "ok"}
